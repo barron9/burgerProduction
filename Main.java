ea0dc4c88873c5c38d33515d6b5d9bac509207bf
berkin.tatlisu@generalmobile.com <000000aa>
2019-04-06 13:49:30 +0300

    init

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-
 import BurgerKingProduction.Burgers.MeatBurger;
 import BurgerKingProduction.Drinks.CocaCola;
 import BurgerKingProduction.Drinks.Lemonade;
@@ -12,17 +11,21 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Starting App!\n\n");
+
         ArrayList<Meal> multimeals = new ArrayList<>();
+
         multimeals.add(
                 new PrepareSingle()
                         .prepareVeganMeal()
                         .addextra(new MeatBurger())
                         .addextra(new CocaCola()));
-        multimeals.add(new PrepareSingle()
-                .prepareMeatMeal()
-                .addextra(new MeatBurger())
-                .addextra(new Lemonade())
-                .addextra(new CocaCola()));
+
+        multimeals.add(
+                new PrepareSingle()
+                        .prepareMeatMeal()
+                        .addextra(new MeatBurger())
+                        .addextra(new Lemonade())
+                        .addextra(new CocaCola()));
 
         System.out.println(new PrepareMulti().totalCost(multimeals));
 
